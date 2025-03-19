31c98f236b334d489e7f15655f2ba7d7c07b9937
akhan197 <A_khan197@yahoo.co.uk>
2025-03-19 17:31:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -129,6 +129,6 @@ def webhook():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 import os
-port = int(os.environ.get("PORT", 8080))  # Match the new port
+port = int(os.environ.get("PORT", 5000))  # Match the new port
 print(f"Starting Flask on port {port}")
 app.run(host="0.0.0.0", port=port)
